8a90c2a0e88842320e0e12afc79bf987c3d395f5
Jannis <github@jh0.eu>
2014-05-04 19:13:46 +0200

    Create loadString.class.php

---
+++ loadString.class.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * @package loadXMLLangString
+ * @author Jannis Hutt
+*/
+
+class String{
+
+	public function loadString($lang, $id){
+	
+		$xml_source = str_replace(array("&amp;", "&"), array("&", "&amp;"), file_get_contents("src/strings.xml"));
+		$string = simplexml_load_string($xml_source);
+		
+		switch($lang){
+			case 'de': //German
+				$load = $string->xpath('/xml/strings[@lang="de"]/string[@id="'.$id.'"]');
+				return ($load[0] ? $load[0] : 'String not found.');
+				break;
+			default:  //Default; load English on default
+				$load = $string->xpath('/xml/strings[@lang="en"]/string[@id="'.$id.'"]');
+				return ($load[0] ? $load[0] : 'String not found.');
+				break;
+		}
+	}
+	
+	public function autoLoadString($id){
+	
+		$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+		
+		$xml_source = str_replace(array("&amp;", "&"), array("&", "&amp;"), file_get_contents("src/strings.xml"));
+		$string = simplexml_load_string($xml_source);
+		
+		switch($lang){
+			case 'de': //German
+				$load = $string->xpath('/xml/strings[@lang="de"]/string[@id="'.$id.'"]');
+				return ($load[0] ? $load[0] : 'String not found.');
+				break;
+			default:  //Default; load English on default
+				$load = $string->xpath('/xml/strings[@lang="en"]/string[@id="'.$id.'"]');
+				return ($load[0] ? $load[0] : 'String not found.');
+				break;
+		}
+	}
+}
+?>
